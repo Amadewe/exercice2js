7cc35e3890cb220aedc3dd69c8d7a20576dc107f
Amadewe <dewevre.amandine@gmail.com>
2019-01-21 09:51:12 +0100

    Fin de l'exercice 2

---
+++ js/JavaScript.js
@@ -0,0 +1,3 @@
+var lastName = 'Nom : Doe\n', firstName = 'Prénom : John\n', city = 'Ville: New-York\n',result;
+result = lastName + firstName + city;
+alert(result); // Affiche : « Nom : Doe ... et le \n permet de revenir à la ligne »
