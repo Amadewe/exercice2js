d1b55c515e089da5197b789b6ee6e1653a334203
Amadewe <dewevre.amandine@gmail.com>
2019-01-21 12:22:54 +0100

    Correction de l'exercice 2

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var lastName = 'Nom : Doe\n';
+var firstName = 'Prénom : John\n';
+var city = 'Ville: New-York\n';
+alert("Nom: " + lastName + "\nPrénom : " +firstName + "\nVille : " +city);
+
+// \n permet de revenir à la ligne »
